6b00bf89a006b875b55bcda584cc5aea87507060
ChA.I <81446209+chaiai@users.noreply.github.com>
2021-05-10 22:46:12 -0400

    Update robot.py

--- chaibot/robot.py
+++ chaibot/robot.py
@@ -24,17 +24,22 @@ class Robot(SingletonConfigurable):
     steering_motor_channel = traitlets.Integer(default_value=3).tag(config=True)
     steering_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
     
+    extra_motor_channel = traitlets.Integer(default_value=4).tag(config=True)
+    extra_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
+    
     def __init__(self, *args, **kwargs):
         super(Robot, self).__init__(*args, **kwargs)
         self.motor_driver = Adafruit_MotorHAT(i2c_bus=self.i2c_bus)
         self.front_motor = Motor(self.motor_driver, channel=self.front_motor_channel, alpha=self.front_motor_alpha)
         self.rear_motor = Motor(self.motor_driver, channel=self.rear_motor_channel, alpha=self.rear_motor_alpha)
         self.steering_motor = Motor(self.motor_driver, channel=self.steering_motor_channel, alpha=self.steering_motor_alpha)
+        self.extra_motor = Motor(self.motor_driver, channel=self.extra_motor_channel, alpha=self.extra_motor_alpha)
         
-    def set_motors(self, front_speed, rear_speed, steering_speed):
+    def set_motors(self, front_speed, rear_speed, steering_speed, extra_motor_speed):
         self.front_motor.value = front_speed
         self.rear_motor.value = rear_speed
         self.steering_motor.value = steering_speed
+        self.extra_motor.value = extra_motor_speed
         
     def forward(self, speed=1.0, duration=None):
         self.front_motor.value = speed
@@ -77,8 +82,17 @@ class Robot(SingletonConfigurable):
         self.steering_motor.value = -speed
         self.front_motor.value = -speed
         self.rear_motor.value = -speed
+        
+    def extra_motor_x(self, speed=1.0):
+        # Spins extra motor in direction x
+        self.extra_motor.value = speed
+    
+    def extra_motor_y(self, speed=1.0):
+        # Spins extra motor in direction y
+        self.extra_motor.value = -speed
 
     def stop(self):
         self.front_motor.value = 0
         self.rear_motor.value = 0
         self.steering_motor.value = 0
+        self.extra_motor.value = 0
