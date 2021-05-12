35ab390912b431c86b712d84e155826323cff22a
ChA.I <81446209+chaiai@users.noreply.github.com>
2021-05-12 15:32:07 -0400

    Update and rename updates/Controller.py to chaibot/ChaibotController.py

--- chaibot/ChaibotController.py
+++ chaibot/ChaibotController.py
@@ -1,3 +1,16 @@
+#########################################################################
+# Most widgets come from the NVIDIA JetBot example Notebooks.
+# My implementation creates a widget view for a Notebook with:
+#
+# 1. Camera instance
+# 2. Motor controls (set to 0.5 speed and without time calls, so the stop button needs to be pressed to stop)
+# 3. Snapshot button (also creates a snapshot directory at chaibot/snapshots
+# 4. Heartbeat and Period sliders
+#
+# Call in Notebook via "from ChaibotController import Controller, camera"
+# [(camera has to be imported in order to call camera.stop()]
+#########################################################################
+
 import os
 import time
 import traitlets
